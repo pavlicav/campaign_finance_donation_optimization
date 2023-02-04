fb46c54df09197d22b1ecb47d57640b5dc5dab4e
Vivian Pavlica <pavlicag@msu.edu>
2023-02-04 16:18:44 -0500

    Small tweaks to get 538 forecast script

--- get_538_forecast.py
+++ get_538_forecast.py
@@ -5,16 +5,18 @@
 import pandas as pd
 
 # Download data from 538 github on close races
-datas = pd.read_csv("https://projects.fivethirtyeight.com/2022-general-election-forecast-data/house_district_toplines_2022.csv")
+datas = pd.read_csv("https://projects.fivethirtyeight.com/2022-general-election-forecast-data/house_district_toplines_2022.csv",
+                     low_memory=False)
 
 # Filter to latest forecast date and choose lite model
-datas = datas.query('forecastdate == "11/8/22"').query('expression == "_lite"')
+latest_date = max(datas.forecastdate.unique())
+datas = datas.query('forecastdate == @latest_date').query('expression == "_lite"')
 
 # Drop unneccecary columns
-sum_data = datas[['cycle', 'branch', 'district', 'forecastdate', 'name_D1',
+sum_data = datas[['branch', 'district', 'forecastdate', 'name_D1',
        'name_R1','winner_D1', 'winner_R1', 'tipping', 'timestamp']]
 
 sum_data = sum_data.dropna() # Drops rows that are non-contested
 
 # Export to CSV
-sum_data.to_csv("latest_forecast.csv")
+sum_data.to_csv("latest_forecast.csv", index=False)
