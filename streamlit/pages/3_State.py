ecd97ad88785cde38de6ebb2d7a8630393aafe5c
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-03-28 14:44:39 -0400

    Update 3_State.py

--- streamlit/pages/3_State.py
+++ streamlit/pages/3_State.py
@@ -189,7 +189,7 @@ def create_card_update(name):
 
 
     <div class="card">
-      <img src="https://github.com/pavlicag/campaign_finance_donation_optimization/main/streamlit/pages/can_pics/person.jpeg" alt="person" style="width:100%">
+      <img src="https://raw.githubusercontent.com/ramseybe/campaign_prototype/main/pages/person.jpeg" alt="person" style="width:100%">
       <h1>person</h1>
       <p class="title">CEO & Founder, Example</p>
       <p></p>
