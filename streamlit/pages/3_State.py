8c425b8307b01fc2b3ba5da577eb4ec06eed5311
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-03-28 14:40:12 -0400

    Update 3_State.py

--- streamlit/pages/3_State.py
+++ streamlit/pages/3_State.py
@@ -28,83 +28,83 @@ for key, val in thing.items():
     needed = val
     break
 
-def create_card(name):
-    file = pd.read_csv('https://raw.githubusercontent.com/ramseybe/hackathon_campaign/main/50_toss_up1.csv')
-    file=file.drop(["Unnamed: 0"],axis=1)
+# def create_card(name):
+#     file = pd.read_csv('https://raw.githubusercontent.com/ramseybe/hackathon_campaign/main/50_toss_up1.csv')
+#     file=file.drop(["Unnamed: 0"],axis=1)
     
-    tempname=name.title()
-    name=name.lower()
+#     tempname=name.title()
+#     name=name.lower()
 
-    person=file.iloc[file.index[file["name"] == tempname].values[0]]
+#     person=file.iloc[file.index[file["name"] == tempname].values[0]]
 
-    card="""<html>
-<head>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 500px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
+#     card="""<html>
+# <head>
+# <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+# <style>
+# .card {
+#   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+#   max-width: 500px;
+#   margin: auto;
+#   text-align: center;
+#   font-family: arial;
+# }
 
-.title {
-  color: grey;
-  font-size: 18px;
-}
-
-
-
-button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-a {
-  text-decoration: none;
-  font-size: 22px;
-  color: black;
-}
-
-button:hover, a:hover {
-  opacity: 0.7;
-}
-</style>
-</head>
-<body>
+# .title {
+#   color: grey;
+#   font-size: 18px;
+# }
 
 
 
-<div class="card">
-  <img src="https://github.com/pavlicag/campaign_finance_donation_optimization/edit/main/streamlit/pages/can_pics/person.jpeg" alt="person" style="width:100%", height=100>
-  <h1>person</h1>
-  <p class="title">district, party</p>
-  <p>Harvard University</p>
-  <div style="margin: 24px 0;">
-  </div>
+# button {
+#   border: none;
+#   outline: 0;
+#   display: inline-block;
+#   padding: 8px;
+#   color: white;
+#   background-color: #000;
+#   text-align: center;
+#   cursor: pointer;
+#   width: 100%;
+#   font-size: 18px;
+# }
+
+# a {
+#   text-decoration: none;
+#   font-size: 22px;
+#   color: black;
+# }
+
+# button:hover, a:hover {
+#   opacity: 0.7;
+# }
+# </style>
+# </head>
+# <body>
+
+
+
+# <div class="card">
+#   <img src="https://github.com/pavlicag/campaign_finance_donation_optimization/edit/main/streamlit/pages/can_pics/person.jpeg" alt="person" style="width:100%", height=100>
+#   <h1>person</h1>
+#   <p class="title">district, party</p>
+#   <p>Harvard University</p>
+#   <div style="margin: 24px 0;">
+#   </div>
   
   
-  <p><button>Donate!</button></p>
-</div>
+#   <p><button>Donate!</button></p>
+# </div>
 
-</body>
-</html>"""
-    #name
-    card=card.replace("person",name)
-    #district
-    card=card.replace("district",person["district"])
-    #party
-    card=card.replace("party",person["party"])
-    return card
+# </body>
+# </html>"""
+#     #name
+#     card=card.replace("person",name)
+#     #district
+#     card=card.replace("district",person["district"])
+#     #party
+#     card=card.replace("party",person["party"])
+#     return card
 
 def get_icons(name,file):
     temp=""""""
