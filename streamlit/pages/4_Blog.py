018f0c46c8167b357048c0cbfb468f613897ae29
nguye734 <62846957+nguye734@users.noreply.github.com>
2023-03-31 13:29:20 -0400

    Update 4_Blog.py

--- streamlit/pages/4_Blog.py
+++ streamlit/pages/4_Blog.py
@@ -42,13 +42,16 @@ st.plotly_chart(advantage,theme=None,use_container_width=True)
 
 
 st.subheader("Against the Odds")
+image1 = Image.open("streamlit/cash_advantage.png")
+st.image(image1)
 st.write(
     """
    Marie Gluesenkamp Perez (D) ran against Joe Kent (R) in the 3rd district of Washington. Perez came from humble backgrounds. Her father was a Mexican immigrant who worked as a salesperson and her mother came from a generational family who worked in the state’s stone-cutting and logging industry. Following her mother’s family legacy, Perez went on to run her own auto repair shop small business with her husband. In total, the couple made under $80,000 per year.
     """
 )
-# image = Image.open("streamlit/combined_disbursement.png")
-# components.html(html_string, height = 400, width = 800)
+image2 = Image.open("streamlit/cash_adv_MGP.png")
+st.image(image2)
+
 st.write("""On the other hand, Perez’s counterpart, Joe Kent, is a well-established political figure within the Republican voting population as he frequently guested on Tucker Carlson Tonight. Kent also has been politically active as he served on the Military Veterans advisory board for former president Donald Trump. As a result, his campaign in 2022 was endorsed by Trump and other prominent figures from the same party. According to OpenSecrets, Kent self-financed almost $250,000 while Perez spent none. 
 
 FiveThirtyEight predicted Perez to only have a 15% winning chance due to Kent’s established presence within the Republican party. Despite her odds, Perez managed to barely raise 2% more than her opponent. As a result, Perez narrowly won the race with 50.14% of the votes with solely donations. 
