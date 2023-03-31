172fa5a2d2e40b2e9aba858c6199f63498d8de7d
nguye734 <62846957+nguye734@users.noreply.github.com>
2023-03-31 15:15:54 -0400

    Update 5_Resources.py

--- streamlit/pages/5_Resources.py
+++ streamlit/pages/5_Resources.py
@@ -5,5 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 #import data
 
+st.set_page_config(page_title='Resources', page_icon=':magnifying_glass:', layout='wide')
+st.write(""" """)
 link = '[GitHub](http://github.com)'
 st.markdown(link, unsafe_allow_html=True)
