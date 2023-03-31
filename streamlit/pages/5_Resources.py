e1ae750fad7ea126a6c1482cf4614d75f18525f9
nguye734 <62846957+nguye734@users.noreply.github.com>
2023-03-31 15:08:25 -0400

    Update 5_Resources.py

--- streamlit/pages/5_Resources.py
+++ streamlit/pages/5_Resources.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import data
+#import data
 
 link = '[GitHub](http://github.com)'
 st.markdown(link, unsafe_allow_html=True)
