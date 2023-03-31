f5e0353bd177b229a8ee21a69181d3eb1c36206a
nguye734 <62846957+nguye734@users.noreply.github.com>
2023-03-31 15:23:31 -0400

    Update 5_Resources.py

--- streamlit/pages/5_Resources.py
+++ streamlit/pages/5_Resources.py
@@ -5,7 +5,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 #import data
 
-st.set_page_config(page_title='Resources', page_icon=':magnifying_glass:', layout='wide')
-st.write(""" """)
-link = '[GitHub](http://github.com)'
-st.markdown(link, unsafe_allow_html=True)
+st.set_page_config(page_title='Resources', page_icon=':glass:', layout='wide')
+st.title('We use data and information from:')
+fec = '[Federal Electoral Commission](https://www.fec.gov/campaign-finance-data/congressional-candidate-data-summary-tables/?year=2022&segment=18)'
+st.markdown(fec, unsafe_allow_html=True)
+opensecrets = '[OpenSecrets](https://www.opensecrets.org/elections-overview)'
+st.markdown(opensecrets, unsafe_allow_html=True)
+
