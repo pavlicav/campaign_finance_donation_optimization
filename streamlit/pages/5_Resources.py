de09fac8ca4f96cc256a48ee2c9048364edca5bc
nguye734 <62846957+nguye734@users.noreply.github.com>
2023-03-31 15:25:09 -0400

    Update 5_Resources.py

--- streamlit/pages/5_Resources.py
+++ streamlit/pages/5_Resources.py
@@ -6,9 +6,17 @@ import plotly.graph_objects as go
 #import data
 
 st.set_page_config(page_title='Resources', page_icon=':glass:', layout='wide')
-st.title('We use data and information from:')
+st.subheader('We use data and information from:')
 fec = '[Federal Electoral Commission](https://www.fec.gov/campaign-finance-data/congressional-candidate-data-summary-tables/?year=2022&segment=18)'
 st.markdown(fec, unsafe_allow_html=True)
 opensecrets = '[OpenSecrets](https://www.opensecrets.org/elections-overview)'
 st.markdown(opensecrets, unsafe_allow_html=True)
+
+st.subheader('Please give us feedback:')
+feedback = '[Feedback](http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html)'
+st.markdown(feedback, unsafe_allow_html=True)
+
+
+
+
 
