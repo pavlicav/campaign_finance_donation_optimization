7d8e484eeed82e38afcf432475fafeeeb811ba8c
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-03-31 15:45:19 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -3,6 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 from PIL import Image
+import numpy as np
 # from streamlit_card import card
 # from IPython.core.display import HTML
 issues=["abortion","gun_control", "climate_change",  "immigration", "healthcare"]
