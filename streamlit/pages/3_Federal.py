186e72013b9e31b87f8fc9155a4388db0cd3d59b
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-05 15:12:50 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -228,6 +228,7 @@ with left_column:
     immigration = st.select_slider('Stance on Immigration',["Pro","Neutral","Anti"],value="Neutral")
     healthcare = st.select_slider('Stance on healthcare',["Pro","Neutral","Anti"],value="Neutral")
     issues = [abortion,guncontrol,climate,immigration,healthcare]
+    st.write(abortion)
     
 
 # checked = get_selected_checkboxes()
