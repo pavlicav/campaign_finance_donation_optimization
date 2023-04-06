e613df860348a81035e9a6a83a972039d7a6e287
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-06 15:21:53 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -143,7 +143,7 @@ def create_card_update(name,i):
     return a
 
 with left_column:
-    states = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+    states = [ 'N/A','AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
            'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
            'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
            'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
