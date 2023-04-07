e4088eec02b2f0d710cd0b05e7a598a73de22612
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-07 17:38:44 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -8,6 +8,13 @@ import numpy as np
 # from streamlit_card import card
 # from IPython.core.display import HTML
 # st.write(st.session_state)
+
+if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+if 'key' not in st.session_state:
+    st.session_state.key = 'value'
+    
+st.write(st.session_state.key)
 issues=["abortion","gun_control", "climate_change",  "immigration", "healthcare"]
 checked=[]
 party=''
