8304b34728deba4224cb43a96607c069ead9968d
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-07 16:19:42 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -1,4 +1,5 @@
 # Libraries
+
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
@@ -6,6 +7,7 @@ from PIL import Image
 import numpy as np
 # from streamlit_card import card
 # from IPython.core.display import HTML
+st.write(st.session_state)
 issues=["abortion","gun_control", "climate_change",  "immigration", "healthcare"]
 checked=[]
 party=''
