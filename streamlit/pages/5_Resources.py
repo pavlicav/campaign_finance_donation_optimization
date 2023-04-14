b849a2f43310a81e4fc4800d32d200d8a3a40948
Greg-Zavalnitskiy <gzavalnitskiy@gmail.com>
2023-04-14 15:26:57 -0400

    Embedding google form

--- streamlit/pages/5_Resources.py
+++ streamlit/pages/5_Resources.py
@@ -3,6 +3,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit.components.v1 as components
+
 #import data
 
 st.set_page_config(page_title='Resources', page_icon=':glass:', layout='wide')
@@ -13,8 +15,10 @@ opensecrets = '[OpenSecrets](https://www.opensecrets.org/elections-overview)'
 st.markdown(opensecrets, unsafe_allow_html=True)
 
 st.subheader('Please give us feedback:')
-feedback = '[Feedback](https://docs.google.com/forms/d/e/1FAIpQLSe7bp8Yx--qFun6QukWljqoH7BjJzTmhxY2iSEMg3fmRyoF0w/viewform?usp=sf_link)'
-st.markdown(feedback, unsafe_allow_html=True)
+# feedback = '[Feedback](https://docs.google.com/forms/d/e/1FAIpQLSe7bp8Yx--qFun6QukWljqoH7BjJzTmhxY2iSEMg3fmRyoF0w/viewform?usp=sf_link)'
+# st.markdown(feedback, unsafe_allow_html=True)
+components.iframe('https://docs.google.com/forms/d/e/1FAIpQLSe7bp8Yx--qFun6QukWljqoH7BjJzTmhxY2iSEMg3fmRyoF0w/viewform?usp=sf_link',width =1000,height = 500,scrolling=True)
+
 
 
 
