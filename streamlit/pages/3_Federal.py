ea1a59dd29bbc6bc70b3a7457e7599a1e3e2f0b6
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-17 15:46:18 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -258,7 +258,7 @@ def get_top_6(user_preferences,state="N/A"):
 #     top_6.to_csv(save_as)
     else:
         temp = positions[positions["district"].str.contains(state)]
-        top_6 = temp.sort_values(by = ["score", "tipping"], ascending = True).iloc[0:6].reset_index(drop = True)
+        top_6 = temp.sort_values(by = ["score", "tipping"], ascending = True).iloc[0::].reset_index(drop = True)
     return top_6
 
 
