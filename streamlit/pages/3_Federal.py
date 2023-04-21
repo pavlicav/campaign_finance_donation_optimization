80ec702c82cf7e11bc5f8b74b0a91a5911fe3d1e
ramseybe <79168952+ramseybe@users.noreply.github.com>
2023-04-21 14:50:56 -0400

    Update 3_Federal.py

--- streamlit/pages/3_Federal.py
+++ streamlit/pages/3_Federal.py
@@ -176,7 +176,8 @@ with left_column:
     temp_list=["Pro-Choice"]+mid+["Pro-Life"]
     abortion = st.select_slider('Stance on Abortion',temp_list,value="Neutral")
     abortion = np_vals[temp_list.index(abortion)]
-    abr= """<span class="fa-stack fa-2x">
+    abr= """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <span class="fa-stack fa-2x">
          <i class="fa fa-gun fa-stack-1x"></i>
         <i class="fas fa-ban fa-stack-2x" style="color:Tomato"></i>
         </span>
